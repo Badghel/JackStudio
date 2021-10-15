39a570cc0cd0e6db882eebd7fb02e635b3d86cf6
Luciano <badghel@hotmail.com>
2021-10-15 11:29:00 -0300

    mejora en la carga

--- js/js.js
+++ js/js.js
@@ -1,30 +1,41 @@
     const arrayImg = ["img1.webp","img2.webp","img3.webp","img4.webp","img5.webp","img6.webp","img7.webp"];
-            let imgAct = 1;
-            console.log(arrayImg)
-            /* Seleccion de botones */
-            let atras = document.getElementById("atras")
-            let adelante = document.getElementById("adelante")
-            let gallery = document.querySelector(".main__gallery");
+    let cont = 0;
+     /* Seleccion de botones */
+   
 
 
-    adelante.addEventListener("click",()=>{
-        imgAct++;
-        cambiarImg()
-        gallery.style.backgroundImage = `url(img/tattoos/img${imgAct}.webp)`
-    })
-    atras.addEventListener("click",()=>{
-        imgAct--;
-        cambiarImg()
-        gallery.style.backgroundImage = `url(img/tattoos/img${imgAct}.webp)`
-    })
-    function cambiarImg(){
-        if(imgAct>arrayImg.length){
-            imgAct=1;
+function carrousel(contenedor){
+    contenedor.addEventListener("click", e=>{
+        let atras = document.getElementById("atras")
+        let adelante = document.getElementById("adelante")
+        let img = document.getElementById("gallery");
+        let tgt = e.target;
+
+
+        if(tgt == atras){
+            if(cont >0){
+                img.src = `img/tattoos/${arrayImg[cont-1]}`;
+                cont--;
+                 
+            }else{
+                img.src =`img/tattoos/${arrayImg[arrayImg.length-1]}`;
+                cont = arrayImg.length-1;
+            }
+        }else if(tgt == adelante){
+            if(cont <arrayImg.length-1){
+                img.src = `img/tattoos/${arrayImg[cont+1]}`;
+                cont++;
+            }else{
+                img.src = `img/tattoos/${arrayImg[0]}`;
+                cont = 0;
+            }
         }
-        if(imgAct<1){ /* No dejamos que imgAct sea -1  */
-            imgAct=arrayImg.length;/* Luego hacemos que sea 3 para que comience desde el 3 hasta la img1 */
-        }
-       
-    }
+    })
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    let contenedor = document.querySelector(".main__gallery");
+    carrousel(contenedor);
+})
 
 
